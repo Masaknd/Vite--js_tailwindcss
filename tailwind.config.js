1d1d4e50c12d7ca917e12f2209403a4d3dbe6770
MasaKnd <masahiro.knd.ds@gmail.com>
2024-06-05 01:06:06 -0400

    all updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,10 +6,16 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     screens: {
-      sm: "480px",
-      md: "768px",
-      lg: "1080px",
-      xl: "1440px",
+      "sm": "480px",
+      // => @media (min-width: 480px) { ... }
+      "md": "768px",
+      // => @media (min-width: 768px) { ... }
+      "lg": "1080px",
+      // => @media (min-width: 1080px) { ... }
+      "xl": "1440px",
+      // => @media (min-width: 1440px) { ... }
+      "2xl":"1536px"
+      // => @media (min-width: 1536px) { ... }
     },
     colors: {
       'primary-regular': '#676767',
@@ -17,7 +23,7 @@ module.exports = {
       'primary-dark': 'rgba(60, 60, 60, 0.9)',
       'primary-trans': 'rgba(103, 103, 103, 0.9)',
       'primary-accent': '#eaeaea',
-      'secondary-regular': '#49b3ba',
+      'secondary-regular':'#8a9596',
       'secondary-light': '#d4eaee',
       'secondary-dark': '#d1dadb',
       'secondary-accent':'#d9e6e7',
